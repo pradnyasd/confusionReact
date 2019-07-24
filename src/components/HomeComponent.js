e90a9c923697d516ef9f1362b49c1a5355af9dc3
akshataw <wagheakshu@gmail.com>
2019-07-24 16:02:09 +0530

    add fetch and error handling

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderCard({ item, isLoading, errMsg }) {
   if(isLoading) {
@@ -14,9 +15,10 @@ function RenderCard({ item, isLoading, errMsg }) {
     )
   }
   else {
+    console.log("Info: " + JSON.stringify(item));
     return (
       <Card>
-        <CardImg src={ item.image } alt={ item.name } />
+        <CardImg width="100%" src={ baseUrl + item.image } alt={ item.name } />
         <CardBody>
           <CardTitle>{ item.name }</CardTitle>
           { item.designation ? <CardSubtitle>{ item.designation }</CardSubtitle> : null }
@@ -39,10 +41,14 @@ function Home(props) {
           />
         </div>
         <div className="col-12 col-md m-1">
-          <RenderCard item={ props.promotion } />
+          <RenderCard
+            item={ props.promotion }
+            isLoading={ props.promosLoading }
+            errMsg={ props.promosErrMsg }
+          />
         </div>
         <div className="col-12 col-md m-1">
-          <RenderCard item={ props.leader } />
+          <RenderCard item={ props.leader }  />
         </div>
       </div>
     </div>
